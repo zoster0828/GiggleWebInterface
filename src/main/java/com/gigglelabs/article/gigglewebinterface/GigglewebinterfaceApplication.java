4710dfb5dde98a459fcdab2a4a70aac476a55656
zoster0828 <zoster0828@gmail.com>
2024-02-21 16:50:28 +0900

    :tada: initial commit

---
+++ src/main/java/com/gigglelabs/article/gigglewebinterface/GigglewebinterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.gigglelabs.article.gigglewebinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigglewebinterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigglewebinterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gigglelabs/article/gigglewebinterface/GigglewebinterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigglelabs.article.gigglewebinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigglewebinterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
